e64aa49d0a64880a36d3f5291b2c69827a097412
quentin1.guyot <quentin1.guyot@epita.fr>
2022-03-10 16:12:50 +0100

    Finish

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -9,7 +9,7 @@ public class LoggerFactory {
         return new CompositeLogger(
             new ContextualLogger(name,
                 new FilteredLogger(
-                    new FileLogger("logs.txt"), conditionFile)),
+                    new FileLogger(System.getProperty("user.home") + "/Desktop/logs.txt"), conditionFile)),
             new ContextualLogger(name,
                     new ConsoleLogger()));
     }
